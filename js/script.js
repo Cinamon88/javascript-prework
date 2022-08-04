a77830b671e26e70a79e67f8dbac4f3372d29558
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-08-04 20:05:52 +0200

    change Game result cod

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ let computerMove = 'nieznany ruch';
 if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (randomNumber == 2){
-    computerMove = 'papier';
+  computerMove = 'papier';
 } else if (randomNumber == 3){
-    computerMove = 'nożyce';
+  computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
@@ -23,31 +23,21 @@ let playerMove = 'nieznany ruch';
 if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2'){
-    playerMove = 'papier';
+  playerMove = 'papier';
 } else if (playerInput == '3'){
-    playerMove = 'nożyce'
+  playerMove = 'nożyce'
 }
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  } else if( computerMove == 'nożyce' && playerMove == 'papier'){
-    printMessage('Komputer wygrywa!');
-  } else if( computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis!');
-  } else if( computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Komputer wygrywa!');
-  } else if( computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-  } else if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis!');
-  } else if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-  } else if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Komputer wygrywa!');
-  } else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis!');
-  } else if( playerMove == 'nieznany ruch'){
-    printMessage('Niedozwolony ruch!');
-  }
+if ((computerMove == 'kamień' && playerMove == 'papier') || 
+    (computerMove == 'nożyce' && playerMove == 'kamień') || 
+    (computerMove == 'papier' && playerMove == 'nożyce')){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == playerMove){
+  printMessage('Remis!');
+} else if (playerMove == 'nieznany ruch'){
+  printMessage('Niedozwolony ruch!');
+} else {
+  printMessage('Komputer wygrywa!');
+}
