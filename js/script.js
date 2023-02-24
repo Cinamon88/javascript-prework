3ba9a6e7a26aab2e96e8826af8f61c4cf48c0b2e
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-24 09:47:02 +0100

    change to english

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 {
-
   const playGame = function(playerInput) {
     clearMessages();
     const getMoveName = function(argMoveId) {
       if (argMoveId == 1) {
-        return 'kamień';
+        return 'stone';
       } else if (argMoveId == 2) {
-        return 'papier';
+        return 'paper';
       } else if (argMoveId == 3) {
-        return 'nożyce';
+        return 'scissors';
       }
       printMessage('Nie znam ruchu o id ' + argMoveId + '.');
       return 'nieznany ruch';
     };
 
     const displayResult = function(argComputerMove, argPlayerMove) {
-      if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
-        (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
-        (argComputerMove == 'papier' && argPlayerMove == 'nożyce')) {
-        printMessage('Ty wygrywasz!');
+      if ((argComputerMove == 'stone' && argPlayerMove == 'paper') ||
+        (argComputerMove == 'scissors' && argPlayerMove == 'stone') ||
+        (argComputerMove == 'paper' && argPlayerMove == 'scissors')) {
+        printMessage('You win!');
       } else if (argComputerMove == argPlayerMove) {
-        printMessage('Remis!');
+        printMessage('Draw!');
       } else {
-        printMessage('Tym razem przegrywasz!');
+        printMessage('You loose!');
       }
     };
 
@@ -32,11 +31,11 @@
 
     console.log('Wylosowana liczba to: ' + randomNumber);
 
-    printMessage('Mój ruch to: ' + ComputerMove);
+    printMessage('My move: ' + ComputerMove);
 
-    console.log('Gracz wpisał: ' + playerInput);
+    console.log('Player move: ' + playerInput);
 
-    printMessage('Twój ruch to: ' + PlayerMove);
+    printMessage('Your move: ' + PlayerMove);
 
     displayResult(ComputerMove, PlayerMove);
   }
